a936702a0f98393a36000dbda34560db8231467a
RayXDD <a0953295757@gmail.com>
2021-10-01 15:09:22 +0800

    專題模板初版

---
+++ 期末專題(MVC)/期末專題(MVC)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace 期末專題_MVC_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
