e4e9d0e135bbd975d45f0d50a2d0dc9814e268af
benny070622 <benny070622@gmail.com>
2021-10-24 19:15:03 +0800

    20211024廣告

---
+++ 廣告/TEST3/TEST3/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TEST3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Create()
+        {
+            ViewBag.ShowA = "A";            
+            ViewBag.ShowB = "B";            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(tAdvertising tAdvertising)
+        {
+            ViewBag.Date = tAdvertising.cADActionDate;        
+            return View();
+        }
+    }
+}
